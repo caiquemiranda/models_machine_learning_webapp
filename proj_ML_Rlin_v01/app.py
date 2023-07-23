3acbd4b4f52b551a066e0410032de2c218bbf20f
caiquemiranda <caiquemirandas@gmail.com>
2023-07-23 20:21:20 -0300

    commit n°018

--- proj_ML_Rlin_v01/app.py
+++ proj_ML_Rlin_v01/app.py
@@ -7,18 +7,16 @@ app = Flask(__name__)
 # Carregar o modelo treinado
 modelo_carregado = joblib.load("modelo_regressao_linear.joblib")
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
-
-@app.route('/prever', methods=['POST'])
-def prever():
-    try:
-        entrada = float(request.form['entrada'])
-        previsao = modelo_carregado.predict(np.array([entrada]).reshape(-1, 1))
-        return render_template('resultado.html', entrada=entrada, previsao=previsao[0])
-    except ValueError:
-        return render_template('erro.html')
+    previsao = None
+    if request.method == 'POST':
+        try:
+            entrada = float(request.form['entrada'])
+            previsao = modelo_carregado.predict(np.array([entrada]).reshape(-1, 1))[0]
+        except ValueError:
+            previsao = "Valor inválido"
+    return render_template('index.html', previsao=previsao)
 
 if __name__ == '__main__':
     app.run(debug=True)

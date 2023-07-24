18fb7c5b1e8c72b0d9ae2d60385bf719b136eaa4
caiquemiranda <caiquemirandas@gmail.com>
2023-07-24 18:16:53 -0300

    commit n°013

--- grafics_models/app.py
+++ grafics_models/app.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template
+import dash
+from dash import dcc, html
 import plotly.graph_objs as go
-
-app = Flask(__name__)
 
 # Dados de exemplo atualizados
 dados_exemplo = {
@@ -10,30 +9,27 @@ dados_exemplo = {
     'vendas_produto_b': [80, 100, 110, 90, 120]
 }
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+# Inicializar a aplicação Dash
+app = dash.Dash(__name__)
 
-@app.route('/grafico')
-def gerar_grafico():
-    meses = dados_exemplo['meses']
-    vendas_produto_a = dados_exemplo['vendas_produto_a']
-    vendas_produto_b = dados_exemplo['vendas_produto_b']
-
-    # Criar o gráfico de barras empilhadas
-    grafico = go.Figure()
-    grafico.add_trace(go.Bar(x=meses, y=vendas_produto_a, name='Produto A'))
-    grafico.add_trace(go.Bar(x=meses, y=vendas_produto_b, name='Produto B'))
-
-    grafico.update_layout(title='Vendas por mês - Produto A vs Produto B',
-                          xaxis_title='Mês',
-                          yaxis_title='Vendas',
-                          barmode='stack')
-
-    # Converter o gráfico para JSON para ser renderizado no frontend
-    grafico_json = grafico.to_json()
-
-    return render_template('grafico.html', grafico_json=grafico_json)
+# Definir layout da aplicação
+app.layout = html.Div([
+    html.H1('Gráfico de Vendas por Mês - Produto A vs Produto B'),
+    dcc.Graph(
+        id='grafico-vendas',
+        figure={
+            'data': [
+                go.Bar(x=dados_exemplo['meses'], y=dados_exemplo['vendas_produto_a'], name='Produto A'),
+                go.Bar(x=dados_exemplo['meses'], y=dados_exemplo['vendas_produto_b'], name='Produto B')
+            ],
+            'layout': go.Layout(
+                xaxis={'title': 'Mês'},
+                yaxis={'title': 'Vendas'},
+                barmode='stack'
+            )
+        }
+    )
+])
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run_server(debug=True)

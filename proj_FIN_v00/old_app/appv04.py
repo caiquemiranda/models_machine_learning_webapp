8c5a13e5cbe3b31866bcad46f1969658408b1c77
caiquemiranda <caiquemirandas@gmail.com>
2023-07-27 19:25:52 -0300

    commit n°014

--- proj_FIN_v00/old_app/appv04.py
+++ proj_FIN_v00/old_app/appv04.py
@@ -15,6 +15,31 @@ acoes = ['AAPL', 'GOOGL', 'MSFT', 'AMZN']
 # Obtendo os dados históricos das ações
 dados_acoes = yf.download(acoes, start='2022-01-01', end='2023-01-01')
 
+# Renomear as colunas para que o nome da ação seja o nível superior das colunas
+dados_acoes.columns = dados_acoes.columns.swaplevel(0, 1)
+dados_acoes = dados_acoes[acoes]  # Filtrar apenas as ações selecionadas
+
+# Criando o aplicativo Flask com Dash
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+
+import yfinance as yf
+import pandas as pd
+import dash
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_table
+from dash.dependencies import Input, Output
+import plotly.graph_objs as go
+from ta.momentum import RSIIndicator
+
+# Lista de ações para análise
+acoes = ['AAPL', 'GOOGL', 'MSFT', 'AMZN']
+
+# Obtendo os dados históricos das ações
+dados_acoes = yf.download(acoes, start='2022-01-01', end='2023-01-01')
+
 # Verificar quais ações têm dados disponíveis
 acoes_com_dados = dados_acoes.columns.get_level_values(0).unique()
 
